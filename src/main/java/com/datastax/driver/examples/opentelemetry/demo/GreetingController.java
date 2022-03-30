d82ba58a53902740d7c211cefdb7a4b50c085f37
margdoc <mikgrzebieluch@gmail.com>
2022-03-30 18:23:22 +0200

    Add basic tracing

--- src/main/java/com/datastax/driver/examples/opentelemetry/demo/GreetingController.java
+++ src/main/java/com/datastax/driver/examples/opentelemetry/demo/GreetingController.java
@@ -5,11 +5,12 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import com.datastax.driver.core.Cluster;
-import com.datastax.driver.examples.opentelemetry.OpenTelemetryConfiguration;
 
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
+import com.datastax.driver.opentelemetry.OpenTelemetryTracingInfoFactory;
+import io.opentelemetry.api.OpenTelemetry;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,13 +21,26 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    private final Cluster cluster = Cluster.builder()
+    private final Cluster cluster;
+
+    private final OpenTelemetryTracingInfoFactory tracingInfoFactory;
+
+    private final Session session;
+
+    public GreetingController() {
+        cluster = Cluster.builder()
             .withoutJMXReporting()
             .withClusterName("ZPP_telemetry")
             .addContactPoint("127.0.0.1")
             .build();
 
-    private final Session session = cluster.connect();
+        OpenTelemetry openTelemetry = OpenTelemetryConfiguration.initializeForZipkin("localhost", 9411);
+
+        tracingInfoFactory = new OpenTelemetryTracingInfoFactory(openTelemetry.getTracer("test"));
+        cluster.setTracingInfoFactory(tracingInfoFactory);
+
+        session = cluster.connect();
+    }
 
 
     @GetMapping("/fetch")
