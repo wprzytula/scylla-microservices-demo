9bc731c2711f11544a041da21b604eafca968cc0
Wojciech Przytuła <wp418383@students.mimuw.edu.pl>
2022-03-30 19:01:52 +0200

    divided into two microservices

---
+++ sender/src/main/java/com/datastax/driver/examples/opentelemetry/demo/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.datastax.driver.examples.opentelemetry.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
---
+++ sender/src/test/java/com/datastax/driver/examples/opentelemetry/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datastax.driver.examples.opentelemetry.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
